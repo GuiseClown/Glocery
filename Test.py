15aa2de8b9679c8a7f1e32bd7382faaa0b56849e
Earth <Guiseclown@gmail.com>
2021-11-14 19:43:05 +0700

    Check err

--- Test.py
+++ Test.py
@@ -3,6 +3,10 @@ from tkinter import *
 import pandas as pd
 import tkinter as tk
 
+# โปรแกรมนี้เมื่อทำการรัน จะให้ใส่รหัสสินค้าซึ่งอยู่ใน excel
+# แล้วทำการตรวจสอบราคาสินค้าของทั้งราคาเก่าและราคาใหม่ใน excel
+# และแสดงผลออกมาพร้อมบอกว่าราคาเท่ากัน,ถูกกว่าหรือแพงกว่าราคาเก่า
+
 #  อ่าน excel
 Newstore = pd.read_excel (r'New.xlsx')
 Oldstore = pd.read_excel (r'Old.xlsx')
@@ -12,17 +16,36 @@ Oldstore = pd.read_excel (r'Old.xlsx')
 # print(Newstore.iloc[1,1])     # row/column
 
 # user input
-print('โปรดใส่รหัสสินค้า:')
-input_a = input()
-input_a = int(input_a)
+while True:
+    while True:
+        try:
+            print("โปรดใส่รหัสสินค้า:")
+            input_a = input()
+            input_a = int(input_a)
+    
+            break
+        except ValueError : #ValueError IndexError
+            print ("โปรดใส่รหัสสินค้าเป็นตัวเลขรหัส")
+
+    try:
+        resultNew = Newstore[Newstore["รหัสสินค้า"] == input_a ]
+        resultOld = Oldstore[Oldstore["รหัสสินค้า"] == input_a ]
+        idNew = resultNew["รหัสสินค้า"]
+        intidNew = int(idNew)
+
+        break
+    except  : 
+        print ("โปรดใส่รหัสสินค้าให้ถูกต้องตามที่มีอยู่")
+         
 
 # แสดงรายการของรหัสสินค้านั้นๆใน cmd
-resultNew = Newstore[Newstore["รหัสสินค้า"] == input_a ]
-resultOld = Oldstore[Oldstore["รหัสสินค้า"] == input_a ]
+# resultNew = Newstore[Newstore["รหัสสินค้า"] == input_a ]
+# resultOld = Oldstore[Oldstore["รหัสสินค้า"] == input_a ]
 
 # ดึงมาแต่ค่าต่างๆของสินค้านั้นๆ
 idNew = resultNew["รหัสสินค้า"]
-# idOld = resultOld["รหัสสินค้า"]
+idOld = resultOld["รหัสสินค้า"]
+
 
 nameNew = resultNew["ชื่อสินค้า"]
 # nameOld = resultOld["ชื่อสินค้า"]
@@ -33,7 +56,7 @@ priceOld = resultOld["ราคา"]
 quantityNew = resultNew["จำนวน"]
 # quantityOld = resultOld["จำนวน"]
 
-intidNew = int(idNew)
+# intidNew = int(idNew)
 tuplenameNew = tuple(nameNew)
 intquantityNew = int(quantityNew)
 intpriceNew = int(priceNew)
@@ -54,7 +77,7 @@ window.title("เปรียบเทียบราคาสินค้า")
 tk.Label(text="เปรียบเทียบราคาสินค้า").grid()
 tk.Label(text=resultNew).grid()
 # tk.Label(text=intidNew ).grid()
-# tk.Label(text=tuplenameNew).grid()
+# tk.Label(text=tuplenameNew).grid()    
 # tk.Label(text=intpriceNew).grid()
 # tk.Label(text=intquantityNew).grid()
 tk.Label(text=state).grid()
